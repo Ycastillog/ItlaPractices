13f6cc56e4e5a7c8fb43cf45035654efbe446aaa
Ycastillog <yeicastillog@hotmail.com>
2025-02-06 22:08:39 -0400

    Loading my first program
    This program determines wheter the number is even or odd.

---
+++ Homework2/Homewod/Homewod.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    // Yeison Castillo 2023-1688
+    static void Main()
+    {
+        // Este es el que solicita al usuario un numero
+        Console.Write("Introduce un numero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Este verifica si par o impar 
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número {numero} es impar.");
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
+
