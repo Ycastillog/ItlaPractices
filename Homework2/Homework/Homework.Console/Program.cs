f57502f0ced20da944661555b3cde081a4681d44
Ycastillog <yeicastillog@hotmail.com>
2025-02-06 22:22:27 -0400

    Loading my first program
    This program determines whether the number is ever or odd.

---
+++ Homework2/Homework/Homework.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    // Yeison Castillo 2023-1688
+    static void Main()
+    {
+        // Solicitar al usuario un número
+        Console.Write("Introduce un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Verificar si el número es par o impar
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+
+            Console.WriteLine("\n Aqui se acaba to'");
+            Console.ReadLine();
+   
+        }
+    }
+}
